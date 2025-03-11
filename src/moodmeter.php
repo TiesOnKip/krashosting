15b9f8211d778c261133a308d214530806f0a23c
TiesOnKip <tiesbergmans@gmail.com>
2025-03-11 09:08:41 +0100

    files

---
+++ src/moodmeter.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mood meter</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/moodmeter.css">
+</head>
+<body>
+    <div class="blokContainer">
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="roodBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="paarsBlok"></div>
+        <div class="blok"></div>
+        <div class="blauwBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="geelBlok"></div>
+        <div class="blok"></div>
+        <div class="zwartBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="roodBlok"></div>
+        <div class="blok"></div>
+        <div class="geelBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="groenBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="zwartBlok"></div>
+        <div class="zwartBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="paarsBlok"></div>
+        <div class="groenBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+        <div class="blauwBlok"></div>
+        <div class="blok"></div>
+        <div class="blok"></div>
+    </div>
+
+    <img src="./img/logo.svg" alt="Yonder" class="logo">
+
+    <div class="moodContainer">
+        <h1 class="title">Hoe was je open dag?</h1>
+    </div>
+</body>
+</html>
