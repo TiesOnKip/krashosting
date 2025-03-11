2a8609cb36caf38dd840e2ad06b308a8ce8961d1
Ianhetkaasje14 <Ianhetkaasje@ziggo.nl>
2025-03-11 10:00:35 +0100

    home

--- src/home.php
+++ src/home.php
@@ -5,12 +5,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/main.css">
-    <link rel="stylesheet" href="./css/home.css">
 </head>
 
 <body>
 
+<<<<<<< Updated upstream
+=======
+    <div class="header">
+        <h1 class="headerTextKlein">Hosting services</h1>
+        <h1 class="headerTextGroot">KRAS HOSTING</h1>
+    </div>
+
+
+    <h1 class="onsProgrammaTitel">Ons Programma</h1>
+    <p class="onsProgrammaText">Onze beste fitness expers helpen je met nieuwe technieken</p>
+
+    <h1 class="vandaagTitel">Vandaag</h1>
+    <p class="vandaagText"> Fijne dag vandaag</p>
+
+    <h1 class="gisterenTitel">Gisteren </h1>
+    <p class="gisterenText"> Fijne dag vandaag/p>
+
+    <div class="beginner">
+        <!-- // hier komt de img van de bloem -->
+        <img src="" alt="">
+        <p class="beginner-text">beginner</p>
+        <p class="beginner-prijs">€5</p>
+    </div>
+    <div class="dynamisch">
+        <!-- // hier komt de img van de bloem -->
+        <img src="" alt="">
+        <p class="dynamisch-text">dynamisch</p>
+        <p class="dynamisch-prijs">€8</p>
+    </div>
+    <div class="gevorderd">
+        <!-- // hier komt de img van de bloem -->
+        <img src="" alt="">
+        <p class="gevorderd-text">gevorderd</p>
+        <p class="gevorderd-prijs">€12</p>
+    </div>
+    <div class="ultimate">
+        <!-- // hier komt de img van de bloem -->
+        <img src="" alt="">
+        <p class="ultimate-text">ultimate</p>
+        <p class="ultimate-prijs">€20</p>
+    </div>
+>>>>>>> Stashed changes
 </body>
 
 </html>
