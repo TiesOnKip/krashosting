2331b39b5f3564a631b80000e6824429ce7e5ac6
TiesOnKip <tiesbergmans@gmail.com>
2025-03-27 13:18:37 +0100

    fonts

--- src/home.php
+++ src/home.php
@@ -3,7 +3,7 @@ session_start();
 
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "root";
+$db_pass = "";
 $db_name = "krashosting";
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
